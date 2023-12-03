be5b6aadf1c947cd31341407f49795602bb6bad0
a-maystorov <alkin971@gmail.com>
2023-12-03 16:21:26 +0100

    done: search section

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    do {
+        const mid = Math.floor(low + (high - low) / 2);
+        const midValue = haystack[mid];
+
+        if (midValue === needle) {
+            return true; // Target found
+        } else if (midValue > needle) {
+            high = mid; // Target is in the left half
+        } else {
+            low = mid + 1; // Target is in the right half
+        }
+    } while (low < high);
+
+    return false; // Target not found
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,27 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    // Determine the jump amount (the distance between two consecutive checks)
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    // Start checking crystal balls with a jump from the beginning
+    let i = jumpAmount;
+    for (; i < breaks.length; i += jumpAmount) {
+        // If a broken crystal ball is found, stop checking
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    // Adjust the index to the previous jump position
+    i -= jumpAmount;
+
+    // Perform a linear search within a smaller range around the last known safe position
+    for (let j = 0; j < jumpAmount && i < breaks.length; ++j, ++i) {
+        // If a broken crystal ball is found, return its position
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    // If no broken crystal ball is found, return -1
+    return -1;
 }
